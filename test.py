7c473415932e20f1ada29fe326a9fecf41c05bec
ibrahim-11 <buyoyaibrahim@gmail.com>
2024-06-25 15:22:56 +0200

    xpath add

--- test.py
+++ test.py
@@ -1,15 +1,23 @@
-from selenium import webdriver 
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
-import time
 
-# creer une instance de webdriver
-driver  = webdriver.Chrome()
-
-# utilisation de l'instance
+driver = webdriver.Chrome()
 driver.get("https://www.selenium.dev/selenium/web/inputs.html")
 
-time.sleep(5)
 
+input = driver.find_element(By.NAME, "no_type")
+input.clear()
+input.send_keys("Anouarfefdiufhisufseiufhsiufhsiefh")
 
+input_bis = WebDriverWait(driver, 10).until(
+  EC.visibility_of_element_located((By.XPATH, "/html/body/form/input[5]"))
+)
+input_bis.clear()
+input_bis.send_keys("Anouarfefdiufhisufseiufhsiufhsiefh")
+
+driver.get_screenshot_as_file("input.png")
 
 driver.quit()
